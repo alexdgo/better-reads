7e4d3d69e3f8f8c72229c7aac7a07b4e672bf11c
staceychen <stacetheace@yahoo.com>
2020-12-03 19:28:37 -0800

    books on display

---
+++ client/src/components/DashboardBookRow.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Media} from 'react-bootstrap';
+
+export default class DashboardBookRow extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	
+	render() {
+		return (
+			<div class="col-3">
+			<div className="book">
+				<Media className="align-items-center justify-content-center">
+                    <img 
+                        height={300}
+                        className="align-self-start mr-3"
+                        src={this.props.book.cover}
+                        alt="cover"
+                    />
+                </Media>
+				<div class="row" text-align="center">
+					<div class="col" >
+						<h7 >{this.props.book.title}</h7>
+						<br></br>
+						<h7 >by {this.props.book.author}</h7>
+						<br></br>
+						<h7 >price ${this.props.book.price}</h7>
+					</div>
+				</div>
+			</div>
+			</div>
+		);
+	}
+}
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,20 +3,20 @@ import '../style/home.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import PageNavbar from './PageNavbar';
 import GenreButton from './GenreButton';
-// import DashboardMovieRow from './DashboardMovieRow';
+import DashboardBookRow from './DashboardBookRow';
 
 export default class Dashboard extends React.Component {
   constructor(props) {
     super(props);
 
     // The state maintained by this React Component. This component maintains the list of genres,
-    // and a list of movies for a specified genre.
+    // and a list of books for a specified genre.
     this.state = {
       genres: [],
-      movies: []
+      books: []
     }
 
-    //this.showMovies = this.showMovies.bind(this);
+    this.showBooks = this.showBooks.bind(this);
   }
 
   // React function that is called when the page load.
@@ -31,9 +31,9 @@ export default class Dashboard extends React.Component {
         // Map each genreObj in genreList to an HTML element:
         // A button which triggers the showMovies function for each genre.
         let genreDivs = genreList.map((genreObj, i) =>
-          <GenreButton id={"button-" + genreObj.genre} onClick={() => this.showMovies(genreObj.genre)} genre={genreObj.genre} />
+          <GenreButton id={"button-" + genreObj.genre} onClick={() => this.showBooks(genreObj.genre)} genre={genreObj.genre} />
         );
-
+        // this.showBooks();
         // Set the state of the genres list to the value returned by the HTTP response from the server.
         this.setState({
           genres: genreDivs
@@ -45,27 +45,29 @@ export default class Dashboard extends React.Component {
 
   /* ---- Q1b (Dashboard) ---- */
   /* Set this.state.movies to a list of <DashboardMovieRow />'s. */
-//   showMovies(genre) {
-//       // Send an HTTP request to the server.
-//       fetch("http://localhost:8081/genres/" + genre, {
-//         method: 'GET' // The type of HTTP request.
-//       })
-//       .then(res => res.json()) // Convert the response data to a JSON.
-//       .then(movieList => {
-//         if (!movieList) return;
-//         // Map each genreObj in genreList to an HTML element:
-//         // A button which triggers the showMovies function for each genre.
-//         let movieDivs = movieList.map((movieObj, i) =>
-//           <DashboardMovieRow id={"row-" + movieObj.title + movieObj.rating} movie={movieObj}/>
-//         );
+  showBooks(genre) {
+      // Send an HTTP request to the server.
+      fetch("http://localhost:8081/genres/" + genre, {
+        method: 'GET' // The type of HTTP request.
+      })
+      .then(res => res.json()) // Convert the response data to a JSON.
+      .then(bookList => {
+        if (!bookList) return;
+        console.log(bookList);
+        // Map each genreObj in genreList to an HTML element:
+        // A button which triggers the showMovies function for each genre.
+        let bookDivs = bookList.map((bookObj, i) =>
+        //   <DashboardBookGrid rows={bookRows}/>
+        <DashboardBookRow id={"row-" + bookObj.title + bookObj.rating} book={bookObj}/>
+        );
 
-//         // Set the state of the genres list to the value returned by the HTTP response from the server.
-//         this.setState({
-//           movies: movieDivs
-//         })
-//       })
-//       .catch(err => console.log(err))	// Print the error if there is one.
-//   }
+        // Set the state of the genres list to the value returned by the HTTP response from the server.
+        this.setState({
+            books: bookDivs
+        })
+      })
+      .catch(err => console.log(err))	// Print the error if there is one.
+  }
 
   render() {    
     return (
@@ -76,25 +78,22 @@ export default class Dashboard extends React.Component {
         <br></br>
         <div className="container movies-container">
           <div className="jumbotron">
-            <div className="h5">Top Movies</div>
+            <div className="h5">Top Books</div>
             <div className="genres-container">
               {this.state.genres}
             </div>
           </div>
 
           <br></br>
-          {/* <div className="jumbotron">
+          <div className="jumbotron">
             <div className="movies-container">
-              <div className="movies-header">
-                <div className="header-lg"><strong>Title</strong></div>
-                <div className="header"><strong>Rating</strong></div>
-                <div className="header"><strong>Vote Count</strong></div>
-              </div>
               <div className="results-container" id="results">
-                {this.state.movies}
+                <div class="row">
+                    {this.state.books}
+                </div>
               </div>
             </div>
-          </div> */}
+          </div>
         </div>
       </div>
     );
