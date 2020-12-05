d354a71e7c6d7505ea6a84dfe7b7a6470ce9b846
ziwen <ziwenhou018@gmail.com>
2020-12-04 21:18:18 -0500

    added some dependencies

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,50 +1,30 @@
-import React, { useState, createContext } from 'react'
-import {
-	BrowserRouter as Router,
-	Route,
-	Switch
-} from 'react-router-dom';
-import Book from './Book';
-import SearchResult from './SearchResult'
-import Home from './Home';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState, createContext } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Book from "./Book";
+import SearchResult from "./SearchResult";
+import Home from "./Home";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-
-export const BookContext = createContext()
+export const BookContext = createContext();
 
 function App() {
-	const [queryResult, setQueryResult] = useState([])
-	const [showResult, setShowResult] = useState(false)
-	return (
-		<BookContext.Provider value = {{ queryResult, setQueryResult, showResult, setShowResult }}>
-			<div className="App">
-				<Router>
-					<Switch>
-						<Route
-							exact
-							path="/"
-							render={() => (
-								<Home />
-							)}
-						/>
-						<Route
-							exact
-							path="/book/:isbn"
-							render={() => (
-								<Book />
-							)}
-						/>
-						<Route
-							exact path="/SearchResult"
-							render={() => (
-								<SearchResult />
-							)}
-						/>
-					</Switch>
-				</Router>
-			</div>
-		</BookContext.Provider>
-	)
+  const [queryResult, setQueryResult] = useState([]);
+  const [showResult, setShowResult] = useState(false);
+  return (
+    <BookContext.Provider
+      value={{ queryResult, setQueryResult, showResult, setShowResult }}
+    >
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route exact path="/" render={() => <Home />} />
+            <Route exact path="/book/:isbn" render={() => <Book />} />
+            <Route exact path="/SearchResult" render={() => <SearchResult />} />
+          </Switch>
+        </Router>
+      </div>
+    </BookContext.Provider>
+  );
 }
 
-export default App
+export default App;
